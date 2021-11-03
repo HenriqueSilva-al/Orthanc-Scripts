61fc34dc1a1525470c720884ceaf0d3bc2c64110
HenriqueSilva-al <59484994+HenriqueSilva-al@users.noreply.github.com>
2021-11-03 17:23:01 -0300

    Add files via upload
    Modifications to the flow of instance forwarding.
    
    Added.:
    Automatic filling of Referring/Performing PhysicianName;
    Automatic standardization of StudyDescription based on InstitutionalDepartamentName;
    
    Modified.:
    Changed SendToModality to more powerfull RestApiPost;

--- sendtomodality.lua
+++ sendtomodality.lua
@@ -1,6 +1,47 @@
-function OnStoredInstance(instanceId, tags, metadata, origin)
-	if origin['RequestOrigin'] ~= 'LuaScript' then
-		PrintRecursive(instanceId)
-		SendToModality(instanceId, 'CEDIM')
+function OnStoredInstance(instanceID, tags, metadata, origin)
+	PrintRecursive(instanceID)
+	if origin['RequestedOrigin'] ~= 'Lua' then
+	local modifiedId
+		if tags['Modality'] == 'CR' then
+			if tags['ReferringPhysicianName'] == '' then
+				local replace = {}
+				replace['ReferringPhysicianName'] = 'MEDICO SOLICITANTE NAO INFORMADO'
+				replace['PerformingPhysicianName'] = 'MEDICO SOLICITANTE NAO INFORMADO'
+				local remove = {}
+				
+				local command = {}
+				command['Replace'] = replace
+				command['Remove'] = remove
+				
+				--PrintRecursive(replace)
+				
+				local modifiedFile = RestApiPost('/instances/' .. instanceID .. '/modify', DumpJson(command, true))
+				
+				--PrintRecursive(DumpJson(command, true))
+				
+				modifiedId = ParseJson(RestApiPost('/instances/', modifiedFile)) ['ID']
+				RestApiDelete('/instances/' .. instanceID)
+				
+				if tags['InstitutionalDepartmentName'] == 'MAMOGRAFIA' then
+				
+					--PrintRecursive(modifiedId)
+					local replace = {}
+					replace['StudyDescription'] = 'MAMOGRAFIA BILATERAL'
+					local remove = {}
+					
+					command = {}
+					command['Replace'] = replace
+					command['Remove'] = remove
+					
+					--PrintRecursive(modifiedId)
+					
+					local modifiedFile = RestApiPost('/instances/' .. modifiedId .. '/modify', DumpJson(command, true))
+					RestApiDelete('/instances/' .. modifiedId)
+					modifiedId = ParseJson(RestApiPost('/instances/', modifiedFile)) ['ID']
+					--PrintRecursive(modifiedId)
+				end
+			end
+			RestApiPost('/modalities/PRORADIS/store', modifiedId)
+		end
 	end
 end
